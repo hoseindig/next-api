c0327569d5d97e1c16afe210cb54804a9039e3c4
h.sheikhi <@h.sheikhi>
2023-04-04 11:32:22 +0330

    clean up

--- pages/api/[...params].js
+++
@@ -1,5 +0,0 @@
-export default function Handler(req, res) {
-  const params = req.query.params;
-  console.log("/api/", "params:", params, req.method);
-  res.status(200).json({ params, message: "multi params" });
-}
--- pages/api/index.js
+++
@@ -1,4 +0,0 @@
-export default function Handler(req, res) {
-  console.log("/api/");
-  res.status(200).json({ name: "test api" });
-}
